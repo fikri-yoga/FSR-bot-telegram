e133baff71611bfaa9a27959e86464ea8e5bc789
fikri-yoga <49784548+fikri-yoga@users.noreply.github.com>
2019-04-19 18:52:10 +0700

    Create Config.php

---
+++ App/config/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST','<place your database host here>');
+define('DB_USER','<place your database user here>');
+define('DB_PASS','<place your database password here>');
+define('DB_NAME','<place your database name here>');
+define('BOT_TOKEN', '<place your telegram bot token here>');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
